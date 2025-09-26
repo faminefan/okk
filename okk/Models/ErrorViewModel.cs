e2534719733572126415bb4559f36ce01d9e6e28
student <student@SCH-A46PC10>
2025-09-26 11:28:57 +0300

    Add project files.

---
+++ okk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace okk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
