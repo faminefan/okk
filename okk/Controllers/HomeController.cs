d067ff06d690ee84a11580ee346fe72925f738e3
STUDENT <STUDENT@SCH-A46PC10>
2025-11-04 15:55:51 +0200

    dasfsdfsdfasd

--- okk/Controllers/HomeController.cs
+++ okk/Controllers/HomeController.cs
@@ -2,12 +2,24 @@ using Microsoft.AspNetCore.Mvc;
 using okk.Models;
 using System.Diagnostics;
 
-namespace WebApp_11Class.Controllers
+namespace okk.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        static List<Ñlothing> clothes = new List<Ñlothing>
 
+            { new Ñlothing { Id = 1, Title = "T-Shirt", Material = "Cotton", Price = 499, Size = "M", Brand = "Nike" },
+            new Ñlothing { Id = 2, Title = "Jeans", Material = "Denim", Price = 1299, Size = "L", Brand = "Levi's" },
+            new Ñlothing { Id = 3, Title = "Jacket", Material = "Leather", Price = 2999, Size = "XL", Brand = "Zara" },
+            new Ñlothing { Id = 4, Title = "Sweater", Material = "Wool", Price = 899, Size = "M", Brand = "H&M" },
+            new Ñlothing { Id = 5, Title = "Shorts", Material = "Cotton", Price = 599, Size = "L", Brand = "Adidas" },
+            new Ñlothing { Id = 6, Title = "Dress", Material = "Silk", Price = 1599, Size = "S", Brand = "Gucci" },
+            new Ñlothing { Id = 7, Title = "Skirt", Material = "Polyester", Price = 799, Size = "M", Brand = "Mango" },
+            new Ñlothing { Id = 8, Title = "Coat", Material = "Wool", Price = 2499, Size = "L", Brand = "Burberry" },
+            new Ñlothing { Id = 9, Title = "Hoodie", Material = "Cotton", Price = 899, Size = "XL", Brand = "Puma" },
+            new Ñlothing { Id = 10, Title = "Blazer", Material = "Linen", Price = 1999, Size = "M", Brand = "Armani" }
+        };
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -22,8 +34,28 @@ namespace WebApp_11Class.Controllers
         {
             return View();
         }
-        wwwwwww
-        public IActionResult actors()
+
+        public IActionResult AdminPanel()
+        {
+            return View(clothes);
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            // delete logic
+            var item = clothes.Find(x => x.Id == id);
+            if (item == null)
+                return NotFound();
+            clothes.Remove(item);
+
+
+            return RedirectToAction("AdminPanel");
+        }
+        public IActionResult Create()
         {
             return View();
         }
---
+++ okk/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace okk.Models
+{
+    public class Сlothing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Material { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+        public string Brand { get; set; }
+        
+    }
+}
