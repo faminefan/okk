d98dc72845e6aac95b171057c3480a6227487846
STUDENT <STUDENT@SCH-A46PC10>
2025-12-09 16:26:37 +0200

    dsadsad

--- okk/Controllers/HomeController.cs
+++ okk/Controllers/HomeController.cs
@@ -9,17 +9,12 @@ namespace okk.Controllers
         private readonly ILogger<HomeController> _logger;
         static List<Ñlothing> clothes = new List<Ñlothing>
 
-            { new Ñlothing { Id = 1, Title = "T-Shirt", Material = "Cotton", Price = 499, Size = "M", Brand = "Nike" },
-            new Ñlothing { Id = 2, Title = "Jeans", Material = "Denim", Price = 1299, Size = "L", Brand = "Levi's" },
-            new Ñlothing { Id = 3, Title = "Jacket", Material = "Leather", Price = 2999, Size = "XL", Brand = "Zara" },
-            new Ñlothing { Id = 4, Title = "Sweater", Material = "Wool", Price = 899, Size = "M", Brand = "H&M" },
-            new Ñlothing { Id = 5, Title = "Shorts", Material = "Cotton", Price = 599, Size = "L", Brand = "Adidas" },
-            new Ñlothing { Id = 6, Title = "Dress", Material = "Silk", Price = 1599, Size = "S", Brand = "Gucci" },
-            new Ñlothing { Id = 7, Title = "Skirt", Material = "Polyester", Price = 799, Size = "M", Brand = "Mango" },
-            new Ñlothing { Id = 8, Title = "Coat", Material = "Wool", Price = 2499, Size = "L", Brand = "Burberry" },
-            new Ñlothing { Id = 9, Title = "Hoodie", Material = "Cotton", Price = 899, Size = "XL", Brand = "Puma" },
-            new Ñlothing { Id = 10, Title = "Blazer", Material = "Linen", Price = 1999, Size = "M", Brand = "Armani" }
-        };
+            { new Ñlothing { Id = 1, Title = "T-Shirt", Material = "Cotton", Price = 499, Size = "M", Brand = "Nike", ImageUrl = "https://picsum.photos/800/600" },
+            new Ñlothing { Id = 2, Title = "Jeans", Material = "Denim", Price = 1299, Size = "L", Brand = "Levi's", ImageUrl = "https://picsum.photos/800/600" },
+            new Ñlothing { Id = 3, Title = "Jacket", Material = "Leather", Price = 2999, Size = "XL", Brand = "Zara", ImageUrl = "https://picsum.photos/800/600" },
+            new Ñlothing { Id = 4, Title = "Sweater", Material = "Wool", Price = 899, Size = "M", Brand = "H&M", ImageUrl = "https://picsum.photos/800/600" },
+            new Ñlothing { Id = 5, Title = "Shorts", Material = "Cotton", Price = 599, Size = "L", Brand = "Adidas", ImageUrl = "https://picsum.photos/800/600" }
+            };
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
--- okk/Models/Product.cs
+++ okk/Models/Product.cs
@@ -8,6 +8,6 @@
         public int Price { get; set; }
         public string Size { get; set; }
         public string Brand { get; set; }
-        
+        public string ImageUrl { get; set; }
     }
 }
